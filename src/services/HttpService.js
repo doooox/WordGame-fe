168763d952b066fb8e336f09498b6bb9e7896d6a
doooox <dtopic12@gmail.com>
2023-05-05 23:16:47 +0200

    fix

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "wordgame-be-production.up.railway.app",
 });
 
 export default httpService;
