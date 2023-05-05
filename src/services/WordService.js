bebe09db37b494ef6df82f8b84d443968b7c18ab
doooox <dtopic12@gmail.com>
2023-05-05 23:32:41 +0200

    .

--- src/services/WordService.js
+++ src/services/WordService.js
@@ -3,7 +3,7 @@ import httpService from "./HttpService";
 const wordService = {
   submitWord: async (word) => {
     try {
-      const response = await httpService.post("words", { word });
+      const response = await httpService.post("api/words", { word });
       return response.data;
     } catch (error) {
       return { score: 0, error: error.message };
